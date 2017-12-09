a95358eba0e3b96b7e04e635cb044878aa0cd9ae
Shay Meredith <shay.e.meredith@gmail.com>
2017-12-09 15:42:29 -0500

    Improve animation reset

--- 02 - JS and CSS Clock/index-my-solution.js
+++ 02 - JS and CSS Clock/index-my-solution.js
@@ -7,15 +7,36 @@ function setDate() {
 
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds/60)*360)+90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    if (secondsDegrees < 91) {
+        document.getElementsByClassName('hand')[2].style.transitionDuration = '0s';
+        secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    }
+    else {
+        document.getElementsByClassName('hand')[2].style.transitionDuration = '0.3s';
+        secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    }
 
     const minutes = now.getMinutes();
     const minutesDegrees = ((minutes/60)*360)+90;
-    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    if (minutesDegrees < 91) {
+        document.getElementsByClassName('hand')[1].style.transitionDuration = '0s';
+        minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    }
+    else {
+        document.getElementsByClassName('hand')[1].style.transitionDuration = '0.3s';
+        minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    }
 
     const hours = now.getHours();
     const hoursDegrees = ((hours/12)*360)+90
-    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    if (hoursDegrees < 91) {
+        document.getElementsByClassName('hand')[0].style.transitionDuration = '0s';
+        hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    }
+    else {
+        document.getElementsByClassName('hand')[0].style.transitionDuration = '0.3s';
+        hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    }
 }
 
 setInterval(setDate, 1000);
